fbab5175e3240221c0526e4909343168ab38cb8b
kmurp62rulz <kmurp62rulz@gmail.com>
2021-04-21 14:46:42 +1000

    Initial Commit

---
+++ Eateries/EateriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EateriesApp.swift
+//  Eateries
+//
+//  Created by Kieran Murphy on 21/4/21.
+//
+
+import SwiftUI
+
+@main
+struct EateriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
