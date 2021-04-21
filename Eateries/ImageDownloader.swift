66d955aaa748c42ded68929ed85b60cc322ab0e0
kmurp62rulz <kmurp62rulz@gmail.com>
2021-04-21 15:16:25 +1000

    .

---
+++ Eateries/ImageDownloader.swift
@@ -0,0 +1,28 @@
+//
+//  ImageDownloader.swift
+//  Eateries
+//
+//  Created by Kieran Murphy on 21/4/21.
+//
+
+import SwiftUI
+
+func imageDownload (_ input: String) -> Image {
+    
+    guard let imageURL = URL(string: input) else {
+        print ("Invalid URL")
+        return (Image("Placeholder"))
+    }
+    
+    guard let imageData = try? Data(contentsOf: imageURL) else {
+        print ("Cannot convert URL to Data")
+        return (Image("Placeholder"))
+    }
+    
+    guard let uiImage = UIImage(data: imageData) else {
+        print ("Cannot convert Data into an image")
+        return (Image("Placeholder"))
+    }
+    let downloadedImage = Image(uiImage: uiImage)
+    return downloadedImage
+}
