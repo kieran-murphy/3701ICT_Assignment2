7ba3b57f26a2f5b391d06b9c54205ac1e5770aba
kmurp62rulz <kmurp62rulz@gmail.com>
2021-04-29 19:54:42 +1000

    Add eaterySetters test

--- EateriesTests/EateriesTests.swift
+++ EateriesTests/EateriesTests.swift
@@ -32,6 +32,39 @@ class EateriesTests: XCTestCase {
             XCTAssertEqual(eatery.notes, notes)
             XCTAssertEqual(eatery.reviews, reviews)
             XCTAssertEqual(eatery.url, url)
+    }
+    
+    
+    func testEaterySetters() {
+            let name = "El Camino Cantina"
+            let location = "Mexico City"
+            let notes = "They make tacos and they give you free sombreros"
+            let reviews = ["Review 1", "Review 2", "Review 3"]
+            let url = "https://www.visitbrisbane.com.au/~/media/articles/2020/november/elcaminocantina_20201125_wide.ashx"
+           
+            var eatery = Eatery(name: name, location: location, notes: notes, reviews: reviews, url: url)
+            
+            let newName = "The Fine Dine"
+            eatery.name = newName
+            XCTAssertEqual(eatery.name, "The Fine Dine")
+            
+            let newLocation = "Lisbon"
+            eatery.location = newLocation
+            XCTAssertEqual(eatery.location, "Lisbon")
+            
+            let newNotes = "Fancy food at its most lavish"
+            eatery.notes = newNotes
+            XCTAssertEqual(eatery.notes, "Fancy food at its most lavish")
+            
+            let newReviews = ["Reviews", "for", "The Fine Dine"]
+            eatery.reviews = newReviews
+            XCTAssertEqual(eatery.reviews, ["Reviews", "for", "The Fine Dine"])
+                
+            let newUrl = "www.url.com"
+            eatery.url = newUrl
+            XCTAssertEqual(eatery.url, "www.url.com")
+            
+            }
 
-}
+
 }
